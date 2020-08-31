1f5af9ccec92c5732f721ca33b4c6089241d867a
Ivan Daniel Maestre Muza <ivan.muza12@gmail.com>
2020-08-31 14:12:07 -0500

    Proyecto prototipo

--- src/ClasesBases/Usuario.java
+++ src/ClasesBases/Usuario.java
@@ -7,18 +7,19 @@ import java.util.ArrayList;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author ivanm
  */
 public class Usuario {
+
     int idUsuario;
     String nombre;
     int idIdiomaMaterno;
     int idIdiomaPreferido;
     int idInteres;
-    ArrayList<Puntaje> puntajes = new ArrayList<>();
+//    ArrayList<Puntaje> puntajes = new ArrayList<>();
+    ArrayList<Integer> puntajes;
 
     public Usuario(int idUsuario, String nombre, int idIdiomaMaterno, int idIdiomaPreferido, int idInteres) {
         this.idUsuario = idUsuario;
@@ -26,6 +27,16 @@ public class Usuario {
         this.idIdiomaMaterno = idIdiomaMaterno;
         this.idIdiomaPreferido = idIdiomaPreferido;
         this.idInteres = idInteres;
+        this.puntajes = new ArrayList<>();
+    }
+
+    public Usuario(int idUsuario, String nombre, int idIdiomaMaterno, int idIdiomaPreferido, int idInteres, ArrayList<Integer> puntajes) {
+        this.idUsuario = idUsuario;
+        this.nombre = nombre;
+        this.idIdiomaMaterno = idIdiomaMaterno;
+        this.idIdiomaPreferido = idIdiomaPreferido;
+        this.idInteres = idInteres;
+        this.puntajes = puntajes;
     }
 
     public int getIdUsuario() {
@@ -68,25 +79,36 @@ public class Usuario {
         this.idInteres = idInteres;
     }
 
-    public ArrayList<Puntaje> getPuntajes() {
+    public ArrayList<Integer> getPuntajes() {
         return puntajes;
     }
 
-    public void setPuntajes(ArrayList<Puntaje> puntajes) {
+    public void setPuntajes(ArrayList<Integer> puntajes) {
         this.puntajes = puntajes;
     }
-    
-    public void añadirPuntaje(Puntaje puntaje){
+
+    public void añadirPuntaje(int puntaje) {
         this.puntajes.add(puntaje);
     }
-    
+
+    public float getPuntaje() {
+        float puntajeTotal = 0;
+        if (puntajes.size() > 0) {
+            for (int p : puntajes) {
+                puntajeTotal = puntajeTotal + p;
+            }
+            puntajeTotal = puntajeTotal / puntajes.size();
+        }
+        return puntajeTotal;
+    }
+
     @Override
     public String toString() {
-        return "##########\n" + 
-                "ID usuario: " + String.valueOf(this.idUsuario) + "\n" +
-                "Nombre: " + this.nombre + "\n" +
-                "Idioma preferido: " + String.valueOf(this.idIdiomaPreferido) + "\n" +
-                "Idioma materno: " + String.valueOf(this.idIdiomaMaterno) + "\n" +
-                "Intereses: " + String.valueOf(this.idInteres) + "\n";
+        return "##########\n"
+                + "ID usuario: " + String.valueOf(this.idUsuario) + "\n"
+                + "Nombre: " + this.nombre + "\n"
+                + "Idioma preferido: " + String.valueOf(this.idIdiomaPreferido) + "\n"
+                + "Idioma materno: " + String.valueOf(this.idIdiomaMaterno) + "\n"
+                + "Intereses: " + String.valueOf(this.idInteres) + "\n";
     }
 }
